a012fa1abd0bf7b4b2fac9ab4c28ed729d9ae883
renejra <rjriveroarrieta@gmail.com>
2022-05-16 15:47:25 +0200

    adding summary

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # mortgage-app
 Streamlit app to calculate stats and monthly payments of a loan
+
+## Installation
+- Create a new conda environment and activate it
+- Install `requirements.txt` and streamlit
+
+## Run
+You can run the app by entering `streamlit run app.py` on a bash terminal
--- app.py
+++ app.py
@@ -9,8 +9,7 @@ st.set_page_config(page_title="The Mortgage App",layout='wide')
 
 # Sidebar messages and menus
 st.sidebar.header('The Mortgage App')
-st.sidebar.write('This app will help you estimate your payments when taking a credit. \
-    Start by entering your loan details below.')
+st.sidebar.write('This app will help you estimate your payments when taking a credit. Start by entering your loan details below.')
 principal = st.sidebar.number_input('Debt', value=100000)
 interest = st.sidebar.number_input('Interest rate % (APR)', value=3.0)/100
 term = st.sidebar.number_input('Term in years', value=10)
@@ -22,10 +21,10 @@ st.sidebar.markdown('Author: [**Rene Rivero**](https://www.linkedin.com/in/renej
 # Working out the data
 loan = mortgage.Loan(principal=principal, interest=interest, term=term)
 am = pd.DataFrame(data=loan._amortize())
-am['rent'] = rent
-am['payment_mit_miete'] = am.payment - am.rent
+am['income'] = rent
+am['payment_w_income'] = am.payment - am.income
 am['cum_payment'] = am.payment.expanding().sum()
-am['rent'] = am.rent.expanding().sum()
+am['income'] = am.income.expanding().sum()
 am = am.apply(pd.to_numeric, errors='ignore')
 am = am.round(decimals=2)
 am['equity'] = am.principal.expanding().sum()
@@ -46,8 +45,28 @@ with col1:
     if rent != 0:
         st.write(f'...minus generated income: {loan.monthly_payment-rent}')
 
-cols = st.multiselect('Show me some graphs on:', am.columns.to_list(), default=['debt', 'interest_paid', 'equity', 'cum_payment'])
+cols = st.multiselect('Show me some graphs on:', am.columns.to_list(), default=['debt', 'interest_paid', 'equity', 'cum_payment', 'income'])
 st.markdown('---')
 st.line_chart(am[cols])
 st.subheader('Amortization plan')
-st.write(am)
+st.write(am.loc[:, am.columns != 'payment_w_income'])
+
+st.header('Summary')
+col1, col2 = st.columns(2)
+with col1:
+    st.write('Original Balance:         {}{:>11,}'.format(loan._currency,loan.principal))
+    st.write('Interest Rate:             {:>11} %'.format(loan.interest))
+    st.write('APY:                       {:>11} %'.format(loan.apy))
+    st.write('APR:                       {:>11} %'.format(loan.apr))
+    st.write('Term:                      {:>11} {}'.format(loan.term, loan.term_unit))
+    st.write('Monthly Payment:          {}{:>11}'.format(loan._currency,loan.monthly_payment))
+
+with col2:
+    st.write('Total principal payments: {}{:>11,}'.format(loan._currency,loan.total_principal))
+    st.write('Total interest payments:  {}{:>11,}'.format(loan._currency,loan.total_interest))
+    st.write('Total interest payments:  {}{:>11,}'.format(loan._currency,loan.total_interest))
+    st.write('Total income:             {}{:>11,}'.format(loan._currency,am.income[-1]- am.income[0]))
+    st.write('Total payments:           {}{:>11,}'.format(loan._currency,loan.total_paid))
+    st.write('Interest to principal:     {:>11} %'.format(loan.interest_to_principle))
+    st.write('Income to principal:       {:>11} %'.format((am.income[-1]- am.income[0])*100/principal))
+    st.write('Years to pay:              {:>11}'.format(loan.years_to_pay))
